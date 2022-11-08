4eaf29251c455016269ce3840f5cc37d8911276d
Stiven Villegas <svillegasc@unal.edu.co>
2022-11-08 11:49:17 -0500

    Elimina codigo comentado

--- src/main/java/org/example/IntefazUsuarioConsola.java
+++ src/main/java/org/example/IntefazUsuarioConsola.java
@@ -11,14 +11,6 @@ public class IntefazUsuarioConsola {
             System.out.println("La suma de 0.1 + 0.2 NO es 0.3");
         }
 
-        //System.out.println(Calculadora.sumar(0.1,0.2));
-
-        /*if(Calculadora.sumar(0.1, 0.2) == 0.3){
-            System.out.println("La suma de 0.1 + 0.2 es 0.3");
-        }else{
-            System.out.println("La suma de 0.1 + 0.2 NO es 0.3");
-        }*/
-
         double d1 = pedirDoublePorTeclado();
         imprimirBonito(d1);
 
@@ -40,32 +32,6 @@ public class IntefazUsuarioConsola {
     public static void imprimirBonito(double resultado){
         System.out.println("El resultado bonito es: " + resultado);
     }
-
-    //Querys no modifican el estado
-    //Commands si lo midifican
-    /*public static void escribirEnArchivo(double d1){
-        if(existeArchivo()){
-            return;
-        }
-
-        ///File(d1)
-    }*/
-
-    /*public static boolean sonIguales(double d1, double d2, double tolerancia){
-        if(Math.abs(d1-d2) < tolerancia){
-            return true;
-        }
-        return false;
-    }*/
-
-    /*public static boolean sonIguales(double d1, double d2, double tolerancia){
-        boolean res = false;
-        if(Math.abs(d1-d2) < tolerancia){
-            res = true;
-        }
-
-        return res;
-    }*/
 
     public static boolean sonIguales(double d1, double d2, double tolerancia){
         boolean res = false;
